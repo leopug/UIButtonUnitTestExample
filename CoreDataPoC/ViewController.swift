9abea2080d8076293f41a96998b4655f5ba06cde
Leonardo Maia Pugliese <leonardo.silva@impalastudios.com>
2022-03-18 07:57:00 +0100

    cleanup

--- CoreDataPoC/ViewController.swift
+++ CoreDataPoC/ViewController.swift
@@ -60,63 +60,6 @@ class ViewController: UIViewController {
     }
 }
 
-//class ViewController: UIViewController {
-//
-//    let counterButton = CustomButton(title:"Button1")
-//    let closureButton = CustomButton(title:"Button2")
-//    var counterButtonAction: ((inout Int) -> ())?
-//    var closureButtonAction: () -> (String)
-//    let textField = UITextField()
-//
-//    private(set) var buttonPressCount = 0
-//
-//    init(button2Action: @escaping () -> (String) = {""}) {
-//        self.closureButtonAction = button2Action
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .white
-//        view.keyboardLayoutGuide.followsUndockedKeyboard = true
-//        configureView()
-//    }
-//
-//    private func configureView() {
-//
-//        [textField].forEach {
-//            view.addSubview($0)
-//            $0.translatesAutoresizingMaskIntoConstraints = false
-//        }
-//
-//        textField.borderStyle = .roundedRect
-//
-//        NSLayoutConstraint.activate([
-//            textField.widthAnchor.constraint(equalToConstant: 200),
-//            textField.heightAnchor.constraint(equalToConstant: 20),
-//            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-//        ])
-//
-//        let textFieldOnKeyboard = view.keyboardLayoutGuide.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 100)
-//        textFieldOnKeyboard.identifier = "textFieldOnKeyboard"
-//        view.keyboardLayoutGuide.setConstraints([textFieldOnKeyboard], activeWhenAwayFrom: .top)
-//    }
-//
-//    @objc
-//    internal func button1Tap() {
-//        counterButtonAction?(&buttonPressCount)
-//    }
-//
-//    @objc
-//    internal func button2Tap() {
-//        print(closureButtonAction())
-//    }
-//}
-
 final class CustomButton: UIButton {
     init(title: String) {
         super.init(frame: .zero)
@@ -133,20 +76,3 @@ final class CustomButton: UIButton {
         setTitle(title, for: .normal)
     }
 }
-
-
-//
-//struct LoginViewModel {
-//    
-//    private let userSubject = PassthroughSubject<User,Never>()
-//    
-//    var userPublisher: AnyPublisher<User,Never> {
-//        userSubject.eraseToAnyPublisher()
-//    }
-//    
-//    func fetchUser() {
-//        
-//        print("The SEND thread: \(Thread.current)")
-//        userSubject.send(User(name: "Mike", age: 32))
-//    }
-//}
